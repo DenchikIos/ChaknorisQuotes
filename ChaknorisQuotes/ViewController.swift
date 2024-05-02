dfdf5ccdec34003894fd09bd4088301996fafe99
Denis IOS Developer <denis.poddubnyy.111@gmail.com>
2024-05-02 15:33:04 +0300

    Initial Commit

---
+++ ChaknorisQuotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChaknorisQuotes
+//
+//  Created by User on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
